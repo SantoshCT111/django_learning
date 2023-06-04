d39798ff9e65ce112ceef1b0f1be207882ee3f84
Work <santoshWork@MacBook-Pro.local>
2023-06-04 15:11:20 +0200

    added some models and did some migrations and added some data to database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class students(models.Model):
+    name = models.CharField(max_length=100)
+    age= models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True,blank=True)
+    
+    
+class Car(models.Model):
+    car_name= models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+    
+    def __str__(self):
+        return self.car_name
+        
+    
